231f24d77ada4adf997e37aaf5d11e3f720b686d
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-04-28 19:41:56 +0200

    Send current board to new connections

--- backend/src/main/java/com/example/backend/GameService.java
+++ backend/src/main/java/com/example/backend/GameService.java
@@ -1,11 +1,20 @@
 package com.example.backend;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
 
 @Service
 public class GameService extends TextWebSocketHandler {
 
+    private String board = "X____";
 
-
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws IOException {
+        System.out.println("Connection established");
+        session.sendMessage(new TextMessage(board));
+    }
 }
