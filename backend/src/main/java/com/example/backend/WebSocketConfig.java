a2da3dc0e27f4b10950a17d8cd9146e88e726a8e
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-04-28 19:33:41 +0200

    Use websocket from frontend

--- backend/src/main/java/com/example/backend/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/WebSocketConfig.java
@@ -15,6 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(gameService, "/ws/game");
+        registry.addHandler(gameService, "/ws/game")
+                .setAllowedOrigins("*");
     }
 }
