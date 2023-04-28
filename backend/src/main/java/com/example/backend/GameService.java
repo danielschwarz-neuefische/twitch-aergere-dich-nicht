f734dbb5f266ab93327fe0bb8c11c452e5313a13
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-04-28 19:24:53 +0200

    Bind /ws/game to a game service

---
+++ backend/src/main/java/com/example/backend/GameService.java
@@ -0,0 +1,11 @@
+package com.example.backend;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Service
+public class GameService extends TextWebSocketHandler {
+
+
+
+}
