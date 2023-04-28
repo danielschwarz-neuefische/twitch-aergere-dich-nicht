aecdb6ebd6c5d1d0f7875eeaa7a064972dfe87e1
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-04-28 21:44:04 +0200

    Print board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,29 @@ import useWebSocket from "react-use-websocket";
 
 export default function App() {
 
-    const [board, setBoard] = React.useState<string>();
+    const [status, setStatus] = React.useState<string>();
+    const [board, setBoard] = React.useState<string[][]>(
+        [
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', 'D', 'D', ' ', ' ', 'O', 'O', 'O', ' ', ' ', 'A', 'A', ' '],
+            [' ', 'D', 'D', ' ', ' ', 'O', 'A', 'O', ' ', ' ', 'A', 'A', ' '],
+            [' ', ' ', ' ', ' ', ' ', 'O', 'A', 'O', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', 'O', 'A', 'O', ' ', ' ', ' ', ' ', ' '],
+            [' ', 'O', 'O', 'O', 'O', 'O', 'A', 'O', 'O', 'O', 'O', 'O', ' '],
+            [' ', 'O', 'D', 'D', 'D', 'D', ' ', 'B', 'B', 'B', 'B', 'O', ' '],
+            [' ', 'O', 'O', 'O', 'O', 'O', 'C', 'O', 'O', 'O', 'O', 'O', ' '],
+            [' ', ' ', ' ', ' ', ' ', 'O', 'C', 'O', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', 'O', 'C', 'O', ' ', ' ', ' ', ' ', ' '],
+            [' ', 'C', 'C', ' ', ' ', 'O', 'C', 'O', ' ', ' ', 'B', 'B', ' '],
+            [' ', 'C', 'C', ' ', ' ', 'O', 'O', 'O', ' ', ' ', 'B', 'B', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ]
+    );
 
     const webSocket = useWebSocket("ws://localhost:8080/ws/game", {
         onOpen: () => console.log("opened"),
         onMessage: (msg) => {
-            setBoard(msg.data);
+            setStatus(msg.data);
         },
     })
 
@@ -19,7 +36,24 @@ export default function App() {
 
     return (
         <div>
-            {board}
+            {status}
+            <pre>
+                {
+                    board.map((row, i) => {
+                        return (
+                            <div key={i}>
+                                {
+                                    row.map((cell, j) => {
+                                        return (
+                                            <span key={j}>{cell}</span>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </pre>
             <button onClick={roleDice}>Role dice</button>
         </div>
     );
