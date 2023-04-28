6d4f0a9434160111dd05364486bcabf0253dec7f
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-04-28 20:35:21 +0200

    Notify all session on dice role

--- backend/src/main/java/com/example/backend/GameSupportService.java
+++ backend/src/main/java/com/example/backend/GameSupportService.java
@@ -2,32 +2,45 @@ package com.example.backend;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class GameSupportService extends TextWebSocketHandler {
 
     private final GameRulesService gameRulesService;
+    private final Set<WebSocketSession> sessions = new HashSet<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws IOException {
         System.out.println("Connection established");
-        publishBoardStatus(session);
+        sessions.add(session);
+        publishBoardStatus();
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        System.out.println("Connection closed");
+        sessions.remove(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         gameRulesService.rollDice();
-        publishBoardStatus(session);
+        publishBoardStatus();
     }
 
-    private void publishBoardStatus(WebSocketSession session) throws IOException {
-        session.sendMessage(new TextMessage(gameRulesService.getBoard()));
+    private void publishBoardStatus() throws IOException {
+        for (WebSocketSession session : sessions) {
+            session.sendMessage(new TextMessage(gameRulesService.getBoard()));
+        }
     }
 }
 
