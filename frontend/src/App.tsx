1d0a5de6e14d33fe9bb9161e844318d4b543b039
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-04-28 22:21:57 +0200

    Display player at starting position

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,6 +22,25 @@ export default function App() {
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         ]
     );
+    const [positions, setPositions] = React.useState<number[][]>(
+        [
+            [7, 0],
+            [7, 1],
+            [7, 2],
+            [7, 3],
+            [7, 4],
+            [8, 4],
+            [9, 4],
+            [10, 4],
+            [11, 4],
+            [11, 5],
+            [11, 6],
+            [10, 6],
+            [9, 6],
+            [8, 6],
+            [7, 6],
+        ]
+    );
 
     const webSocket = useWebSocket("ws://localhost:8080/ws/game", {
         onOpen: () => console.log("opened"),
@@ -37,19 +56,28 @@ export default function App() {
     return (
         <div>
             {status}
-            <div id="board">
-                {
-                    board.map((row, i) => {
-                        return row.map((cell, j) => {
-                            return (
-                                <div className="cell" key={j} style={{
-                                    gridColumn: j + 1,
-                                    gridRow: i + 1,
-                                }}>{cell}</div>
-                            )
+            <div id="board-and-player">
+                <div id="board">
+                    {
+                        board.map((row, i) => {
+                            return row.map((cell, j) => {
+                                return (
+                                    <div className="cell" key={j} style={{
+                                        gridColumn: j + 1,
+                                        gridRow: i + 1,
+                                    }}>{cell}</div>
+                                )
+                            })
                         })
-                    })
-                }
+                    }
+                </div>
+                <div id="player" style={{
+                    left: (positions[0][0]) * 40,
+                    top: (positions[0][1] + 1) * 40,
+                    color: 'blue',
+                }}>
+                    ●
+                </div>
             </div>
             <button onClick={roleDice}>Role dice</button>
         </div>
